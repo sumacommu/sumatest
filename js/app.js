f64da5c7c4dc44a062cae7a8b3793fb4920d473d
arolia <arowana4401@gmail.com>
2025-03-25 00:50:28 +0900

    Vercel用にcallbackURLを更新

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: 'https://test.vercel.app/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
