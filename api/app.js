4c596c3744722931c206fe60c7f619a6bc9aaa01
sumacommu <sumacommu@gmail.com>
2025-05-15 06:13:34 +0900

    A

--- api/app.js
+++ api/app.js
@@ -437,8 +437,8 @@ app.get('/api/solo/check', async (req, res) => {
       <html>
         <head>
           <link rel="stylesheet" href="/css/general.css">
-          <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
-          <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"></script>
+          <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+          <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
         </head>
         <body>
           <div class="container">
@@ -455,14 +455,16 @@ app.get('/api/solo/check', async (req, res) => {
                 </div>
               </form>
               <script>
-                firebase.initializeApp({
+                var firebaseConfig = {
                   apiKey: "${process.env.FIREBASE_API_KEY}",
                   authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
                   projectId: "${process.env.FIREBASE_PROJECT_ID}",
                   storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
                   messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
-                  appId: "${process.env.FIREBASE_APP_ID}"
-                });
+                  appId: "${process.env.FIREBASE_APP_ID}",
+                  measurementId: "${process.env.FIREBASE_MEASUREMENT_ID}"
+                };
+                firebase.initializeApp(firebaseConfig);
                 const db = firebase.firestore();
                 const userId = "${userId}";
                 const matchesRef = db.collection('matches');
